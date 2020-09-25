7e0452ae11626e0964e71f4effdfb3539eca2730
Belobopka <belobaka1@gmail.com>
2020-09-26 01:08:26 +0300

    init

---
+++ precache-manifest.1ab0e468dbbb9673c77342d9c653c85a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90845272a0011398c7fdf0cf44765f04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d14a2968bb940d4696f",
+    "url": "/static/css/main.b516f92a.chunk.css"
+  },
+  {
+    "revision": "c48849d655fff6abf816",
+    "url": "/static/js/3.8920f7b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.8920f7b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2f367e15936dab35f32",
+    "url": "/static/js/AnotherPage.4dd00b18.chunk.js"
+  },
+  {
+    "revision": "1d14a2968bb940d4696f",
+    "url": "/static/js/main.e0c66933.chunk.js"
+  },
+  {
+    "revision": "3253130e96ccf423eea4",
+    "url": "/static/js/runtime-main.1af36a77.js"
+  }
+]);
